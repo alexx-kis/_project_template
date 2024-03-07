a7b2c97123fb3352d571d331ce3b6e296c232e22
alexx-kis <kislov@advline.ru>
2024-03-07 12:54:15 +0300

    open modal function modified

--- js/modals.js
+++ js/modals.js
@@ -6,34 +6,44 @@ import { isEscapeKey } from "./utils.js";
 const openModal = (modal) => {
 	modal.classList.add('open');
 
-	const cross = modal.querySelector('.modal__cross');
-
-	// коллбэк по нажатию на крестик
+	//# обработчик закрытия модального окна по клику на крестик
 	const onCrossClick = () => {
 		closeModal(modal);
-		cross.removeEventListener('click', onCrossClick);
 	}
-	cross.addEventListener('click', onCrossClick);
 
-	// коллбэк по нажатию на escape
+	//# обработчик закрытия модального окна по нажатию на escape
 	const onEscapeKeydown = (e) => {
 		if (isEscapeKey(e)) {
 			closeModal(modal);
 		}
-		document.removeEventListener('keydown', onEscapeKeydown);
 	}
+	//# обработчик закрытия модального окна по нажатию на него само
+	const onModalClick = () => {
+		closeModal(modal);
+	}
+
+	const cross = modal.querySelector('.modal__cross');
+
+	cross.addEventListener('click', onCrossClick);
 	document.addEventListener('keydown', onEscapeKeydown);
+	modal.addEventListener('click', onModalClick);
 
-	// фиксация страницы при открытом модальном окне
-	document.body.style.overflow = 'hidden';
-}
+	document.body.style.overflow = 'hidden'; // disable document scroll
 
-/*$// $------------ close modal ------------$ //$*/
-const closeModal = (modal) => {
-	modal.classList.remove('open');
-	document.body.style.overflow = 'visible';
+
+
+	/*$// $------------ close modal ------------$ //$*/
+	const closeModal = (modal) => {
+		modal.classList.remove('open');
+
+		cross.removeEventListener('click', onCrossClick);
+		document.removeEventListener('keydown', onEscapeKeydown);
+		modal.removeEventListener('click', onModalClick);
+
+		document.body.style.overflow = 'visible'; // enable document scroll
+	}
 }
 
 
 /*&// &======================== EXPORT ========================& //&*/
-export { openModal, closeModal }
+export { openModal }
