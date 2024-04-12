26fe8b1fdacfcfbfe84a66e77fb0bfe2e06e5df7
alexx-kis <kislov@advline.ru>
2024-04-12 10:39:19 +0300

    fix

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,9 @@
-//$ функция возвращает true, если нажата клавиша Escape
+//$ function returns true, if Escape-key was pressed
 const isEscapeKey = (event) => event.key === 'Escape'
 
+//$ function converts px-value to vw-value (without unit)
+const convertPxToVw = (value) => value / window.innerWidth * 100;
 
 /*&// &======================== EXPORT ========================& //&*/
 
-export {isEscapeKey}
+export {isEscapeKey, convertPxToVw}
