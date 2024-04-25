4aa14a40f444c8b3f8f45ea2cf5be37f01b7654b
alexx-kis <kislov@advline.ru>
2024-04-25 12:43:28 +0300

    scss structure modified

--- js/selects.js
+++ js/selects.js
@@ -7,14 +7,28 @@ selects.forEach(select => {
   const selectLabels = select.querySelectorAll('.select__label');
   const selectContent = select.querySelector('.select__content');
 
-  selectPane.addEventListener('click', () => {
+  selectPane.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const activeSelect = document.querySelector('.select._active');
+
     if (select.classList.contains('_active')) {
       select.classList.remove('_active');
       selectContent.style.maxHeight = null;
     } else {
       select.classList.add('_active');
       selectContent.style.maxHeight = selectContent.scrollHeight + 'px';
+
+      if (activeSelect) {
+        activeSelect.classList.remove('_active');
+        const activeContent = activeSelect.querySelector('.select__content');
+        activeContent.style.maxHeight = null;
+      }
     }
+  });
+
+  document.addEventListener('click', () => {
+    select.classList.remove('_active');
+    selectContent.style.maxHeight = null;
   });
 
   selectLabels.forEach(selectLabel => {
