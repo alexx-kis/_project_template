ad6f3430e2fe58d7c8d1cd0bd8dbd0e2a0b81370
alexx-kis <kislov@advline.ru>
2024-06-24 17:02:42 +0300

    + drag file field

---
+++ js/drag-file-field.js
@@ -0,0 +1,56 @@
+const initDragFileField = (field) => {
+  const input = field.querySelector('.drag-file-field__input');
+  const icon = field.querySelector('.drag-file-field__icon');
+  const text = field.querySelector('.drag-file-field__text');
+  const thumbnail = field.querySelector('.drag-file-field__thumbnail');
+
+  const handleFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      thumbnail.src = e.target.result;
+      if (icon) {
+        icon.style.display = 'none';
+      }
+      if (text) {
+        text.style.display = 'none';
+      }
+      if (thumbnail) {
+        thumbnail.style.display = 'block';
+      }
+    };
+    reader.readAsDataURL(file);
+  }
+
+  input.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      handleFile(file);
+    }
+  });
+
+  field.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    field.classList.add('dragover');
+  });
+
+  field.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    field.classList.remove('dragover');
+  });
+
+  field.addEventListener('drop', (e) => {
+    e.preventDefault();
+    field.classList.remove('dragover');
+    const file = e.dataTransfer.files[0];
+    if (file) {
+      input.files = e.dataTransfer.files;
+      handleFile(file);
+    }
+  });
+}
+
+
+const dragFilesFields = document.querySelectorAll('.drag-file-field');
+dragFilesFields.forEach(dragFilesField => {
+  initDragFileField(dragFilesField);
+})
