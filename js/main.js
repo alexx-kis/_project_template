4d440b2025b5a4817242ed95d43c4f6accfc9f76
alexx-kis <kislov@advline.ru>
2024-07-04 16:12:32 +0300

    cleared

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*&// &======================== IMPORT ========================& //&*/
-import './utils.js';
-import './modals.js';
-import './custom-selects.js';
-import './slide-dropdown.js';
+// import './utils.js';
+// import './modals.js';
+// import './custom-selects.js';
+// import './slide-dropdown.js';
 // import './accordion.js';
 // import './drag-file-field.js';
