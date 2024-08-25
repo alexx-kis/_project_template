e6ce02b528f24a3fc418aaf997fa5efdd500ac6b
Alexander <fenaethylaamiinloever@gmail.com>
2024-08-25 18:33:14 +0300

    headings boreders

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,11 @@
 // $======================== UTILS ========================$ //
 
-//$ function returns true, if Escape-key was pressed
-const isEscapeKey = (event) => event.key === 'Escape'
+// $ function returns true, if Escape-key was pressed
+const isEscapeKey = (event) => event.key === 'Escape';
 
-//$ function converts px-value to vw-value (without unit)
+// $ function converts px-value to vw-value (without unit)
 const convertPxToVw = (value) => value / window.innerWidth * 100;
 
-/*&// &======================== EXPORT ========================& //&*/
+/*& // &======================== EXPORT ========================& // &*/
 
-export {isEscapeKey, convertPxToVw}
+export { isEscapeKey, convertPxToVw };
