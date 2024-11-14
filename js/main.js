deb9b02666badc7874f5b2ec01d2c0838b62b164
alexx-kis <kislov@advline.ru>
2024-11-14 15:38:04 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // import './utils.js';
 // import './modals.js';
 // import './custom-selects.js';
-// import './dropdowns-and-selects';
+// import './dropdowns-and-selects.js';
 // import './tooltips.js';
 // import './slide-dropdown.js';
 // import './accordion.js';
